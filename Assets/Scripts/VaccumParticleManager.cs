4ece3b0b641ff2d8e54d553d4a3dc57f9663778a
THAXTERIA-PC <39074621+SquallzFunk@users.noreply.github.com>
2019-12-08 21:38:25 -0500

    Did some particle effect stuff for art assignment

---
+++ Assets/Scripts/VaccumParticleManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaccumParticleManager : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem vacPart;
+    [SerializeField] private ParticleSystem.ShapeModule vacShape;
+    [SerializeField] private ParticleSystem.MainModule vacMain;
+
+    [SerializeField] private float startLifeTime;
+    [SerializeField] private float hemRadius;
+    [SerializeField] private float time = 0.1f;
+
+    void Start()
+    {
+        vacPart = GetComponent<ParticleSystem>();
+        vacShape = vacPart.shape;
+        vacMain = vacPart.main;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (vacPart.isEmitting)
+        {
+            if (time > 0.1f && hemRadius < 5f)
+            {
+                startLifeTime += 0.03f;
+                hemRadius += 0.08f;
+                time = 0;
+            }
+
+            vacMain.startLifetime = startLifeTime;
+            vacShape.radius = hemRadius;
+        }
+        else if (!vacPart.isEmitting)
+        {
+            vacMain.startLifetime = 0.2f;
+            vacShape.radius = 2;
+            hemRadius = 2;
+        }
+    }
+}
