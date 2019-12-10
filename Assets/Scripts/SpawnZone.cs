9eb7677321e667b20f845704e451f76921586edf
THAXTERIA-laptop <39074621+HeavySqualls@users.noreply.github.com>
2019-12-09 22:34:33 -0500

    - Tweaked wind colliders a bit - Added respawn functionality - Built two demos - 1 for controller and 1 for keyboard & mouse

---
+++ Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<WindOnUnit>().respawnZone = this.transform;
+        }
+    }
+}
