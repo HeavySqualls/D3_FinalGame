40a50e9e158b8b6f2792fb1e859db6c9c9eeea4a
THAXTERIA <39074621+THAXTERIA@users.noreply.github.com>
2020-03-10 22:31:58 -0400

    ADDED: - Cinemachine & Timeline Packages - cinematics for scene 1

---
+++ Assets/Cinematics/CInematicTriggerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class CInematicTriggerController : MonoBehaviour
+{
+    public PlayableDirector timeLine;
+    public GameObject cinematicCam;
+    PlayerController pCon;
+    CircleCollider2D circColl;
+
+    void Start()
+    {
+        circColl = GetComponent<CircleCollider2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && cinematicCam.activeSelf == true)
+        {
+            cinematicCam.SetActive(false);
+
+            if (pCon != null)
+            {
+                pCon.EnablePlayerController();
+                pCon = null;
+            }
+
+            circColl.enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        pCon = other.gameObject.GetComponent<PlayerController>();
+        RollingObject ro = other.GetComponent<RollingObject>();
+
+        if (pCon != null || ro != null)
+        {
+            timeLine.Play();
+            cinematicCam.SetActive(true);
+
+            if (pCon != null)
+            {
+                pCon.DisablePlayerController();
+            }
+        }
+    }
+}
