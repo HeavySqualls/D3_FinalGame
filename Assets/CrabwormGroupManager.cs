5f4377f8949c029afe4ca841c9ad59656f674f5e
THAXTERIA-PC <39074621+SquallzFunk@users.noreply.github.com>
2020-05-11 15:11:26 -0400

    ADDED: - Add in a crab worm group manager to control the individual attacks on the player from each crabworm at one time - Moved Enemies scripts folder to more visible location in the project files

---
+++ Assets/CrabwormGroupManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabwormGroupManager : MonoBehaviour
+{
+    public List<CrabwormLarvaController> crabwormGroup = new List<CrabwormLarvaController>();
+    CrabwormLarvaController attackingCrabworm;
+    public PlayerController pCon;
+
+    private bool isAttacking = false;
+    public bool isPlayerInZone;
+
+    private void Update()
+    {
+        if (isPlayerInZone && crabwormGroup.Count > 0) // Track and coordinate attacks 
+        {
+            for (int i = 0; i < crabwormGroup.Count; i++)
+            {
+                if (crabwormGroup[i].isDead)
+                {
+                    crabwormGroup.Remove(crabwormGroup[i]);
+                }
+                else if (crabwormGroup[i] != attackingCrabworm && crabwormGroup[i].currentState != Enemy_Base.State.DoNothing)
+                {
+                    crabwormGroup[i].currentState = Enemy_Base.State.DoNothing;
+
+                    if (crabwormGroup[i].target == null)
+                    {
+                        crabwormGroup[i].target = pCon.gameObject;
+                    }
+
+                    crabwormGroup[i].isUnitPaused = true;
+                    crabwormGroup[i].FaceTarget();
+                }
+            }
+
+            if (!isAttacking)
+            {
+                ChooseEnemyToAttack();
+            }
+        }
+    }
+
+    private void ChooseEnemyToAttack()
+    {
+        isAttacking = true;
+
+        //// Random Choice
+        //isAttacking = true;
+        //int randomEnemy = Random.Range(0, crabwormGroup.Count);
+
+        //attackingCrabworm = crabwormGroup[randomEnemy];
+
+        float shortestDist = 0;
+
+        // Choosing closest enemy
+        for (int i = 0; i < crabwormGroup.Count; i++)
+        {
+            float currentDist = Vector3.Distance(crabwormGroup[i].transform.position, pCon.transform.position);
+
+            if (crabwormGroup[i] == crabwormGroup[0] || currentDist < shortestDist)
+            {
+                shortestDist = currentDist;
+                attackingCrabworm = crabwormGroup[i];
+            }
+        }
+
+        attackingCrabworm.isUnitPaused = false;
+        attackingCrabworm.currentState = Enemy_Base.State.Hunting;
+
+        StartCoroutine(AttackAndCooldown());
+    }
+
+    public float attackDelay = 0.3f;
+
+    private IEnumerator AttackAndCooldown()
+    {
+        yield return new WaitForSeconds(attackDelay);
+
+        attackingCrabworm.isUnitPaused = true;
+        attackingCrabworm.currentState = Enemy_Base.State.DoNothing;
+        isAttacking = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CrabwormLarvaController crabCon = collision.gameObject.GetComponent<CrabwormLarvaController>();
+
+        if (crabCon != null)
+        {
+            if (!crabwormGroup.Contains(crabCon) && !crabCon.isDead)
+            {
+                crabwormGroup.Add(crabCon);
+            }
+        }
+
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            print("fuck");
+            isPlayerInZone = true;
+            pCon = collision.gameObject.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<CrabwormLarvaController>())
+        {
+            CrabwormLarvaController crabCon = collision.GetComponent<CrabwormLarvaController>();
+            crabwormGroup.Remove(crabCon);
+        }
+        else if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            isPlayerInZone = false;
+            print("why");
+            foreach (CrabwormLarvaController waitingcrabCon in crabwormGroup)
+            {
+                waitingcrabCon.isUnitPaused = false;
+                waitingcrabCon.currentState = Enemy_Base.State.Patrolling;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Enemy_Turret_Base.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy_Turret_Base : MonoBehaviour
+{
+    public enum State { Idle, Prone, Attacking, Hurt, Dead }
+
+    [Space]
+    [Header("--- ENEMY TURRET BASE ---")]
+
+    public State startingState;
+    [SerializeField] protected State currentState;
+
+    public bool isTurretPaused = false;
+    protected bool isIdle;
+    protected bool isPatrolling;
+    protected bool isHunting;
+    public bool isDead;
+    protected bool isInAirInWind;
+    protected bool isHit = false;
+    protected bool isHurt = false;
+    protected bool isCooldown = false;
+    protected bool objectHit = false;
+
+    [Space]
+    [Header("Unit Health:")]
+
+    [Tooltip("The current amount of health on the unit.")]
+    [SerializeField] public float currentHP;
+    [Tooltip("The starting health of the unit.")]
+    [SerializeField] float startHP = 10f;
+
+    [Space]
+    [Header("Enemy Combat:")]
+
+    [Tooltip("The damage this enemy will cause to the player.")]
+    public int damageOutput = 2;
+    [Tooltip("The time between attacks on the player.")]
+    public float attackCooldown = 0.5f;
+    [Tooltip("The distance the player will be knocked back when hit by the player. (total distance is relative to the duration of the stun time variable)")]
+    [SerializeField] protected float knockBack;
+    [Tooltip("The distance the player will be knocked in tp the air when hit by this unit. (total distance is relative to the duration of the stun time variable)")]
+    [SerializeField] protected float knockUp;
+    [Tooltip("The amount of time the player will be stunned when hit by the unit.")]
+    [SerializeField] protected float stunTime;
+
+    [Space]
+    [Header("Raycasts:")]
+
+    [Tooltip("The location of the units eye range.")]
+    [SerializeField] protected Transform eyeRange;
+    [Tooltip("The distance of the units eye range.")]
+    [SerializeField] protected float eyeRangeDistance = 10f;
+
+    [Space]
+    [Header("Enemy References:")]
+    protected Vector2 direction;
+    protected LayerMask playerLayerMask;
+    protected Animator animator;
+    public GameObject target;
+
+    private void OnEnable()
+    {
+        SpawnManager.onResetLevelObjects += ResetTurretUnit;
+    }
+
+    private void OnDisable()
+    {
+        SpawnManager.onResetLevelObjects -= ResetTurretUnit;
+    }
+
+    protected virtual void Start()
+    {
+        Toolbox.GetInstance().GetLevelManager().AddTurretEnemies(this);
+
+        currentState = startingState;
+
+        currentHP = startHP;
+        animator = GetComponent<Animator>();
+
+        playerLayerMask = ((1 << 12));
+
+        // Set the enemy in the correct direction at Start
+        if (direction == Vector2.zero)
+            direction = Vector2.right;
+    }
+
+    protected virtual void Update()
+    {
+        // Dealt with in the relative enemy controller
+    }
+
+    public void ResetTurretUnit()
+    {
+        isDead = false;
+        Debug.Log(gameObject.name + " was reset");
+        gameObject.SetActive(true);
+        currentState = startingState;
+        currentHP = startHP;
+    }
+
+    // << --------------------------------------- COMBAT -------------------------------- >> //
+
+    protected virtual void AfterThisUnitWasAttacked()
+    {
+        // Behaviour handled in specific enemy controller
+    }
+
+    protected void AfterAttackCooldown()
+    {
+        // Behaviour handled in specific enemy controller
+    }
+
+    protected virtual void KillUnit()
+    {
+        // Behaviour handled in specific enemy controller
+    }
+
+    public void ThisUnitHit()
+    {
+        AfterThisUnitWasAttacked();
+    }
+
+    // << ------------------------------------- FLIP SPRITE -------------------------------- >> //
+
+    protected void FlipSprite()
+    {
+        if (direction == Vector2.right)
+        {
+            transform.eulerAngles = new Vector3(0, -180, 0);
+            direction = Vector2.left;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            direction = Vector2.right;
+        }
+    }
+}
